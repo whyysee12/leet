fd3dcedb70322156b65e6e28fbc554485d2ad19c
Yatharth Chhabra <yatharthchhabra12@gmail.com>
2026-09-04 15:19:50 +0530

    Time: 38 ms (24.02%) | Memory: 84.1 MB (16.19%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
@@ -2,13 +2,15 @@ class Solution {
     public int maxIceCream(int[] costs, int coins) {
         Arrays.sort(costs);
         int sum=0;
-        int c=0;
+        int count=0;
         for(int i=0;i<costs.length;i++){
             sum+=costs[i];
-            if(sum<=coins) c++;
-            else break;
+           // count++;
+            if(sum>coins) break;
+            count++;
+            
         }
-        return c;
+        return count;
         
     }
 }
